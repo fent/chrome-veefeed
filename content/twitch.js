54961fa92587217c6dcd2dbf9ab6e684f3eb68e4
fent <933490+fent@users.noreply.github.com>
2016-04-21 18:22:41 -0400

    fix checking if twitch video ended

--- content/twitch.js
+++ content/twitch.js
@@ -16,6 +16,14 @@ function getSlider(callback) {
   }, 1000);
 }
 
+// Converts from 00:00:00 or 00:00 to seconds.
+function timeToSeconds(str) {
+  var s = str.split(':');
+  return s.length === 2 ?
+    ~~s[0] * 60 + ~~s[1] :
+    ~~s[0] * 3600 + ~~s[1] * 60 + ~~s[2];
+}
+
 getSlider(function() {
   var $currentTime = document.getElementsByClassName('js-seek-currenttime')[0];
   var $totalTime = document.getElementsByClassName('js-seek-totaltime')[0];
@@ -26,11 +34,13 @@ getSlider(function() {
   // However, there's a bug where the slider will move without the video
   // playing. Happens when the page is opened at a small size.
   var observer = new MutationObserver(function() {
-    var currentTime = $currentTime.textContent;
-    totalTime = totalTime || $totalTime.textContent;
-    if (currentTime === totalTime && currentTime !== '00:00') {
-      chrome.runtime.sendMessage({ ended: true });
+    var currentTime = timeToSeconds($currentTime.textContent);
+
+    // Sometimes the timer will end 1 second off.
+    totalTime = totalTime || timeToSeconds($totalTime.textContent) - 1 || 1;
+    if (totalTime && currentTime >= totalTime) {
       observer.disconnect();
+      chrome.runtime.sendMessage({ ended: true });
     }
   });
   observer.observe($currentTime, { childList: true });
