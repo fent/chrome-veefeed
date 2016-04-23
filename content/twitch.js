3c9e31312331691da71866b1f02a342bbf449ab6
fent <933490+fent@users.noreply.github.com>
2016-04-23 19:39:45 -0400

    more reliable method of checking if twitch video ended

--- content/twitch.js
+++ content/twitch.js
@@ -16,32 +16,20 @@ function getSlider(callback) {
   }, 1000);
 }
 
-// Converts from 00:00:00 or 00:00 to seconds.
-function timeToSeconds(str) {
-  var s = str.split(':');
-  return s.length === 2 ?
-    ~~s[0] * 60 + ~~s[1] :
-    ~~s[0] * 3600 + ~~s[1] * 60 + ~~s[2];
-}
-
 getSlider(function() {
-  var $currentTime = document.getElementsByClassName('js-seek-currenttime')[0];
-  var $totalTime = document.getElementsByClassName('js-seek-totaltime')[0];
-  var totalTime;
+  var $player = document.getElementsByClassName('player')[0];
   
   // This looks at the player slider, whenever it reaches the end,
   // it assumes the video has ended.
   // However, there's a bug where the slider will move without the video
   // playing. Happens when the page is opened at a small size.
   var observer = new MutationObserver(function() {
-    var currentTime = timeToSeconds($currentTime.textContent);
-
-    // Sometimes the timer will end 1 second off.
-    totalTime = totalTime || timeToSeconds($totalTime.textContent) - 1 || 1;
-    if (totalTime && currentTime >= totalTime) {
+    // If the player has ended, not just paused, it will have its
+    // `data-ended` attribute be equal to `true`.
+    if ($player.getAttribute('data-ended') === 'true') {
       observer.disconnect();
       chrome.runtime.sendMessage({ ended: true });
     }
   });
-  observer.observe($currentTime, { childList: true });
+  observer.observe($player, { attributes: true });
 });
