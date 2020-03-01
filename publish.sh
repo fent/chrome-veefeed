10ea578b65c070044e9fb7d65baf1d5f5070a804
fent <933490+fent@users.noreply.github.com>
2020-03-01 07:37:32 -0500

    chore(cd): add chrome store upload workflow
    credits to videospeed extension

--- publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# https://github.com/DrewML/chrome-webstore-upload-cli
-# npm install -g chrome-webstore-upload-cli
-webstore upload --extension-id elpfmfdcigaklhenpknoifgfcfnefkdf --auto-publish "$@"
